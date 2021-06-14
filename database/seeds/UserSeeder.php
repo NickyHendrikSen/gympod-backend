1b68be8f2b597f1230c85b213d041357b7cf146b
Nicky Hendrik Sen <nicky.sen@binus.edu>
2021-06-14 09:47:45 +0700

    User Seeder

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -15,6 +15,25 @@ class UserSeeder extends Seeder
             'user_name' => 'admin',
             'phone' => '081231231231',
             'password' => Hash::make('admin'),
+            'is_admin' => true
+        ]);
+        DB::table('users')->insert([
+            'user_name' => 'John',
+            'phone' => '081111111111',
+            'password' => Hash::make('john'),
+            'is_admin' => false
+        ]);
+        DB::table('users')->insert([
+            'user_name' => 'Doe',
+            'phone' => '0822222222222',
+            'password' => Hash::make('doe'),
+            'is_admin' => false
+        ]);
+        DB::table('users')->insert([
+            'user_name' => 'Mark',
+            'phone' => '083333333333',
+            'password' => Hash::make('mark'),
+            'is_admin' => false
         ]);
     }
 }
